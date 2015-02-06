9fa507bd15102f24b62a2a202b5828e2ab3be1ad
javasavvy <javasavvydev@gmail.com>
2015-02-05 19:14:28 -0800

    Decided to return the actual model object from the rest controller, rather than creating dto objects

--- src/main/java/com/myreloader/manager/BookManager.java
+++ src/main/java/com/myreloader/manager/BookManager.java
@@ -2,12 +2,13 @@ package com.myreloader.manager;
 
 import com.myreloader.manager.util.BaseCrudManager;
 import com.myreloader.model.Book;
+import com.myreloader.model.BookImpl;
 
 /**
  * Created by Justin on 2/5/2015.
  */
 
-public interface BookManager extends BaseCrudManager<Book>{
+public interface BookManager extends BaseCrudManager<BookImpl>{
 
-    Iterable<Book> findBooksByAuthor(String author);
+    Iterable<BookImpl> findBooksByAuthor(String author);
 }
